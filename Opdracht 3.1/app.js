1016c51dd0cbefb0c49e78ae9dc0f4df9b2fa351
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2022-09-15 15:07:15 +0200

    javascript 3.1

--- Opdracht 3.1/app.js
+++ Opdracht 3.1/app.js
@@ -6,3 +6,39 @@ document.write(myName)
 document.write(myAge)
 document.write(diploma)
 document.write(passed) 
+
+let num1 = 70;
+let num2 = 69;
+
+let anwser1 = num1 + num2
+console.log(anwser)
+
+let anwser2 = num1 - num2;
+console.log(answer1)
+
+let num3 = 70;
+let num4 = 69;
+
+let anwser3 = num3 + num4
+console.log(anwser)
+
+let anwser4 = num3 - num4;
+console.log(answer1)
+
+let num5 = 70;
+let num6 = 69;
+
+let anwser5 = num5 + num6
+console.log(anwser)
+
+let anwser6 = num5 - num6;
+console.log(answer1)
+
+let num7 = 70;
+let num8 = 69;
+
+let anwser = num7  num8
+console.log(anwser)
+
+let answer1 = num7 - num8;
+console.log(answer1)
