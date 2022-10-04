0e1ce76a4d8b8296df0b70ba92b75e0b7f4a03f1
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2022-10-04 11:55:13 +0200

    6.2

---
+++ opdracht 6.2/app.js
@@ -0,0 +1,9 @@
+// document.getElementById("title").style.color = "red";
+
+function kleur_Aanpassen() {
+    
+document.getElementById("title").style.color = "red";
+document.getElementById("title").innerText = "dit is javascript en jason is soms erg vervelend"
+
+}
+
