ddbfeaa5177eff57df5e9073c1af3a3ae0a13175
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2022-11-01 11:36:35 +0100

    opdracht 7.1 js

---
+++ 7.1/app.js
@@ -0,0 +1,17 @@
+let budget = 100;
+let product = 60; 
+
+let par = document.getElementById("paragraaf");
+
+if (budget > product){
+console.log("Ik heb genoeg geld");
+par.innerText ="Ik heb genoeg geld";
+par.style.color = "green";
+}
+else{
+    console.log("Ik heb te weinig geld");
+    par.innerText ="Ik heb te weinig geld";
+    par.style.color = "red";
+    par.style
+
+}
