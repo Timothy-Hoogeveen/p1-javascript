9a263101a3a150f2503d57974acd505e2101e5e7
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2022-11-09 14:24:30 +0100

    8.2

--- 8.1/app.js
+++ 8.1/app.js
@@ -3,30 +3,45 @@ let input1 = document.getElementById("input1")
 let input2 = document.getElementById("input2")
 
 function multiply(){
-  let answer =  input1.value * input2.value
 
-  titel.innerText = answer
+    if (input1.value > 0 && input2.value > 0 ) {
+
+        let answer =  input1.value * input2.value
+
+        titel.innerText = answer
+    } else {
+        titel.innerText = "getal is te laag"
+    }
+
 
 
 }
 
 function minus(){
+
+    if (input1.value > 0 && input2.value > 0 ){
         let answer =  input1.value - input2.value
       
         titel.innerText = answer
+        
+    }
+      
 
 }
 
-function add(){
-    let answer =  Number(input1.value) + Number(input2.value)
+function add()
+ 
+      if (input1.value > 0 && input2.value > 0 ){
+        let answer =  input1.value - input2.value
       
-    titel.innerText = answer
-
+        titel.innerText = answer
 }
 
-function devide(){
-    let answer =  input1.value / input2.value
+function devide()
+
+    if (input1.value > 0 && input2.value > 0 ){
+        let answer =  input1.value - input2.value
       
-    titel.innerText = answer
+        titel.innerText = answer
 
 }
